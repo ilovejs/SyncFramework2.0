32bfb49d2fe3843a5d75f5e76a3c3c42ec1b027e
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2014-10-15 10:00:13 +1100

    working code: sql server to sql server.

--- SyncSQLServerAndSQLCompact/ExecuteCompactSync/Program.cs
+++ SyncSQLServerAndSQLCompact/ExecuteCompactSync/Program.cs
@@ -14,27 +14,20 @@ namespace ExecuteCompactSync
     {
         static void Main(string[] args)
         {
-            // 创建到SyncCompactDB database的连接
-            SqlCeConnection clientConn = new SqlCeConnection(@"Data Source='E:\SyncCompactDB.sdf'");
+            var clientConn = new SqlConnection("Data Source=IT34;Initial Catalog=SyncDBClient;User ID=sa;Password=ISAsql07");
 
-            // 创建到SyncDB服务器数据库的连接
-//            SqlConnection serverConn = new SqlConnection("Data Source=IT34; Initial Catalog=SyncDBMaster; Integrated Security=True");
-            SqlConnection serverConn = new SqlConnection("Data Source=IT34;Initial Catalog=SyncDBMaster;User ID=sa;Password=ISAsql07");
+            var serverConn = new SqlConnection("Data Source=IT34;Initial Catalog=SyncDBMaster;User ID=sa;Password=ISAsql07");
 
             // 创建同步代理（sync orhcestrator）
-            SyncOrchestrator syncOrchestrator = new SyncOrchestrator();
-
-            // 设置同步代理的本地同步提供程序为SyncCompactDB compact database的ProductsScope
-            syncOrchestrator.LocalProvider = new SqlCeSyncProvider("ProductsScope", clientConn);
-
-            // 设置同步代理的远程同步提供程序为SyncDB server database的ProductsScope
-            syncOrchestrator.RemoteProvider = new SqlSyncProvider("ProductsScope", serverConn);
-
-            // 设置同步会话的同步方向为Upload和Download
-            syncOrchestrator.Direction = SyncDirectionOrder.UploadAndDownload;
+            var syncOrchestrator = new SyncOrchestrator
+            {
+                LocalProvider = new SqlSyncProvider("ProductsScope", clientConn),
+                RemoteProvider = new SqlSyncProvider("ProductsScope", serverConn),
+                Direction = SyncDirectionOrder.UploadAndDownload
+            };
 
             // 侦听本地同步提供程序的ApplyChangeFailed事件
-            ((SqlCeSyncProvider)syncOrchestrator.LocalProvider).ApplyChangeFailed += new EventHandler<DbApplyChangeFailedEventArgs>(Program_ApplyChangeFailed);
+            ((SqlSyncProvider)syncOrchestrator.LocalProvider).ApplyChangeFailed += Program_ApplyChangeFailed;
 
             // 执行同步过程
             SyncOperationStatistics syncStats = syncOrchestrator.Synchronize();
@@ -49,10 +42,8 @@ namespace ExecuteCompactSync
 
         static void Program_ApplyChangeFailed(object sender, DbApplyChangeFailedEventArgs e)
         {
-            // 显示冲突类型
             Console.WriteLine(e.Conflict.Type);
 
-            // 显示错误信息
             Console.WriteLine(e.Error);
         }
 
